0d3a1eac121821064058fac7126c009bddfe1b38
Aniket panchal <aniketpanchal07@gmail.com>
2024-12-18 02:11:36 +0530

    done with post,put and delete

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.DTOs.stocks;
 using api.Mappers;
 using api.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -39,6 +40,56 @@ namespace api.Controllers
                 return NotFound();
             }
             return Ok(stock.ToStockDTO());
+
+        }
+
+        // create stock
+        [HttpPost]
+        public IActionResult Create([FromBody] StockRequestDto stockDTO)
+        {
+            var stockModel = stockDTO.ToStockFromCreateDTO();
+            _context.Stocks.Add(stockModel);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDTO());
+
+        }
+
+        // update the stock
+        [HttpPut]
+        [Route("{id:int}")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+            stockModel.Symbol = updateDto.Symbol;
+            stockModel.CompanyName = updateDto.CompanyName;
+            stockModel.Purchase = updateDto.Purchase;
+            stockModel.LastDiv = updateDto.LastDiv;
+            stockModel.Industry = updateDto.Industry;
+            stockModel.MarketCap = updateDto.MarketCap;
+
+            _context.SaveChanges();
+            return Ok(stockModel.ToStockDTO());
+
+        }
+
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.Stocks.Remove(stockModel);
+            _context.SaveChanges();
+            return Ok(stockModel.ToStockDTO());
 
         }
 
--- api/Mappers/StockMapper.cs
+++ api/Mappers/StockMapper.cs
@@ -9,20 +9,33 @@ namespace api.Mappers
 {
     public static class StockMapper
     {
-        public static StockDTOs ToStockDTO(this Stock StockModel)
+        public static StockDTOs ToStockDTO(this Stock stockModel)
         {
             return new StockDTOs
             {
-                Id = StockModel.Id,
-                Symbol = StockModel.Symbol,
-                CompanyName = StockModel.CompanyName,
-                Purchase = StockModel.Purchase,
-                LastDiv = StockModel.LastDiv,
-                Industry = StockModel.Industry,
-                MarketCap = StockModel.MarketCap,
-                // Comments= StockModel.Comments.Select( c => c.ToCommentDto()).ToList()
+                Id = stockModel.Id,
+                Symbol = stockModel.Symbol,
+                CompanyName = stockModel.CompanyName,
+                Purchase = stockModel.Purchase,
+                LastDiv = stockModel.LastDiv,
+                Industry = stockModel.Industry,
+                MarketCap = stockModel.MarketCap,
+                // Comments= stockModel.Comments.Select( c => c.ToCommentDto()).ToList()
             };
 
+        }
+
+        public static Stock ToStockFromCreateDTO(this StockRequestDto stockDto)
+        {
+            return new Stock
+            {
+                Symbol = stockDto.Symbol,
+                CompanyName = stockDto.CompanyName,
+                Purchase = stockDto.Purchase,
+                LastDiv = stockDto.LastDiv,
+                Industry = stockDto.Industry,
+                MarketCap = stockDto.MarketCap
+            };
         }
 
     }
