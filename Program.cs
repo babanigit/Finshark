cf4bc152b973916182cc6154602349e94e78b68d
Aniket Panchal <aniket.panchal@healthindiatpa.com>
2026-04-13 18:27:39 +0530

    stoped auto migrated , commented a line

--- Program.cs
+++ Program.cs
@@ -99,7 +99,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    db.Database.Migrate();
+    // db.Database.Migrate();
 }
 
 // ── React static files ─────────────────────────────────────────────────────────
